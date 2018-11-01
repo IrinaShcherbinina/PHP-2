6f4d24cdac5ba8dd1f657a2c0cb35855a16ff593
IrinaShcherbinina <42144602+IrinaShcherbinina@users.noreply.github.com>
2018-11-01 05:41:53 +0300

    lesson7

--- lesson6/public/index.php
+++ lesson6/public/index.php
@@ -1,7 +1,7 @@
 <?php
 include "../config/main.php";
 include "../services/Autoloader.php";
-include "../vendor/autoload.php";
+//include "../vendor/autoload.php";
 
 spl_autoload_register([new app\services\Autoloader(), 'loadClass']);
 
